8cdb49648b335292d92bdbab581542379fc60778
Thibault Viaene <thibault.viaene@student.odisee.be>
2023-10-12 11:29:47 +0200

    sommeren, met tracing

---
+++ labo3/src/main/java/Sommeren.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * LABO3-RECURSIE : M
+ *
+ * @author viaen
+ * @version 12/10/2023
+ */
+public class Sommeren {
+    static int som = 0;
+    public static boolean vindSomSet(int controleGetal, int[] getallen, List<Integer> subset, int index) {
+        if (controleGetal == 0) {
+            return true;
+        } else if (controleGetal < 0 || index == getallen.length) {
+            return false;
+        } else {
+            List<Integer> newSubset = new ArrayList<>(subset);
+            newSubset.add(getallen[index]);
+            System.out.println(newSubset);
+            return vindSomSet(controleGetal - getallen[index], getallen, newSubset, index + 1) ||
+            vindSomSet(controleGetal, getallen, subset, index + 1);
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
