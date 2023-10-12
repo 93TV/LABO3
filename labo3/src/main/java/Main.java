a09fd00e75a96ce5ef481974629ad616d0143d58
tuprec <thibe.provost@student.odisee.com>
2023-10-12 08:35:33 +0200

    hello

---
+++ labo3/src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * LABO3:Main
+ *
+ * @author thibe
+ * @version 12/10/2023
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
