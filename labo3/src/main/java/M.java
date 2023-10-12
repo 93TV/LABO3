36c0b1742709aff182e55f792ad170513a05fb1d
Thibault Viaene <thibault.viaene@student.odisee.be>
2023-10-12 08:58:33 +0200

    M & MTEST

---
+++ labo3/src/main/java/M.java
@@ -0,0 +1,12 @@
+/**
+ * LABO3-RECURSIE : M
+ *
+ * @author viaen
+ * @version 12/10/2023
+ */
+public class M {
+
+    static boolean m(int[] getallen) {
+        
+    }
+}
---
+++ labo3/src/test/java/MTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * LABO3-RECURSIE : MTest
+ *
+ * @author viaen
+ * @version 12/10/2023
+ */
+class MTest {
+
+}
